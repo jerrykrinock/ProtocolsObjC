e9c4138f30c8ff7b5c0eae6f78a0f9e202c974e4
Jerry Krinock <jerry@sheepsystems.com>
2014-01-12 21:41:06 -0800

    Found a couple dozen files which were in my BkmkMgrs project directory that should have been in my reusable code folders.  Very tedious to move them to proper locations, then re-pointing in Xcode.  I hope I didn’t screw anything up.  Also looks like a couple checkboxes for new files which I neglected to switch on in previous commit.

---
+++ SSYOwnee.h
@@ -0,0 +1,12 @@
+/*!
+ @brief    Protocol for objects which have an id 'owner' property
+ */
+@protocol SSYOwnee
+
+/*!
+ @brief    An object, loosely defined as an owner of some kind, but
+ actually you can use it for whatever you want to.
+ */
+- (id)owner ;
+
+@end
